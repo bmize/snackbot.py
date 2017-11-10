9d708046c24136018ebedf6bf68e18427c70aea5
bmize14 <19563823+bmize14@users.noreply.github.com>
2017-11-10 02:49:22 -0800

    Removed lenny command from rng group and added roll command

--- snackbot/discord_client/cogs/rng.py
+++ snackbot/discord_client/cogs/rng.py
@@ -3,20 +3,25 @@
 Bot commands involving random chance
 """
 
-from discord.ext import commands
+from collections import namedtuple
 import random
+from discord.ext import commands
+
+from discord_client.utils import dnd
 
 
 class RNG:
     def __init__(self, bot):
         self.bot = bot
 
+    """
     @commands.group(pass_context=True)
     async def rng(self, ctx):
         if ctx.invoked_subcommand is None:
             await self.bot.say('Invalid random subcommand passed.')
+    """
 
-    @rng.command()
+    @commands.command()
     async def lenny(self):
         """Displays a random lenny face."""
         lenny = random.choice([
@@ -25,6 +30,17 @@ class RNG:
             "ヽ༼ຈل͜ຈ༽ﾉ"
         ])
         await self.bot.say(lenny)
+
+    @commands.command()
+    async def roll(self, die_info):
+        """Displays the result of rolling count dice with count sides"""
+        Die = namedtuple('Dice', 'num sides')
+        try:
+            die = Die(*dnd.parse_roll_syntax(die_info))
+            rolls = dnd.roll_dice(die.num, die.sides)
+            await self.bot.say(', '.join(rolls))
+        except TypeError:
+            await self.bot.say('Incorrect die format. Sample format: !roll 2d6')
 
 
 def setup(bot):
