0998cc1ba11bad54a7864adb56c32790cfebc5b3
bmize14 <19563823+bmize14@users.noreply.github.com>
2018-03-23 23:07:06 -0700

    Error message update

--- snackbot/discord_client/cogs/reddit.py
+++ snackbot/discord_client/cogs/reddit.py
@@ -18,19 +18,26 @@ class Reddit:
 
     @r.command()
     async def pic(self, subreddit_name: str):
-        submission_url = fetch.get_random_media_submission(self.bot, subreddit_name)
-        if submission_url is None:
-            await self.bot.say("No results found. Please select media-heavy subreddits only for this command.")
-        else:
-            await self.bot.say(submission_url)
+        try:
+            submission_url = fetch.get_random_media_submission(self.bot, subreddit_name)
+            if submission_url is None:
+                await self.bot.say("No results found. Please select media-heavy subreddits only for this command.")
+            else:
+                await self.bot.say(submission_url)
+        except:
+            await self.bot.say("No results found. Subreddit is likely private/quarantined/non-existent/etc.")
 
     @r.command()
     async def search(self, subreddit_name: str, search_str: str):
-        submission_url = fetch.get_random_submission_by_title(self.bot, subreddit_name, search_str)
-        if submission_url is None:
-            await self.bot.say("something went wrong")
-        else:
-            await self.bot.say('https://www.reddit.com' + submission_url)
+        try:
+            submission_url = fetch.get_random_submission_by_title(self.bot, subreddit_name, search_str)
+            if submission_url is None:
+                await self.bot.say("Something went wrong :(")
+            else:
+                await self.bot.say('https://www.reddit.com' + submission_url)
+        except:
+            await self.bot.say("An error has occurred. Search parameters might be bad. Subreddit also might be"
+                               " private/quarantined/non-existent/etc.")
 
 
 def setup(bot):
